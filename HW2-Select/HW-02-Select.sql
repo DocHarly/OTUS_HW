781bd6133af321050214dacd42705b94135717d8
Alexander Treboganov <docharly@yandex.ru>
2022-09-28 23:18:34 +0300

    Create HW-02-Select.sql

---
+++ HW2-Select/HW-02-Select.sql
@@ -0,0 +1,122 @@
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+Занятие "02 - Оператор SELECT и простые фильтры, JOIN".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД WideWorldImporters можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+1. Все товары, в названии которых есть "urgent" или название начинается с "Animal".
+Вывести: ИД товара (StockItemID), наименование товара (StockItemName).
+Таблицы: Warehouse.StockItems.
+*/
+
+select si.StockItemID, si.StockItemName from Warehouse.StockItems si
+where si.StockItemName like '%urgent%' or si.StockItemName like 'Animal%'
+
+/*
+2. Поставщиков (Suppliers), у которых не было сделано ни одного заказа (PurchaseOrders).
+Сделать через JOIN, с подзапросом задание принято не будет.
+Вывести: ИД поставщика (SupplierID), наименование поставщика (SupplierName).
+Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders.
+По каким колонкам делать JOIN подумайте самостоятельно.
+*/
+
+select * from Purchasing.Suppliers s
+left join Purchasing.PurchaseOrders po on s.SupplierID = po.SupplierID
+where po.PurchaseOrderID is NULL
+
+/*
+3. Заказы (Orders) с ценой товара (UnitPrice) более 100$ 
+либо количеством единиц (Quantity) товара более 20 штук
+и присутствующей датой комплектации всего заказа (PickingCompletedWhen).
+Вывести:
+* OrderID
+* дату заказа (OrderDate) в формате ДД.ММ.ГГГГ
+* название месяца, в котором был сделан заказ
+* номер квартала, в котором был сделан заказ
+* треть года, к которой относится дата заказа (каждая треть по 4 месяца)
+* имя заказчика (Customer)
+Добавьте вариант этого запроса с постраничной выборкой,
+пропустив первую 1000 и отобразив следующие 100 записей.
+
+Сортировка должна быть по номеру квартала, трети года, дате заказа (везде по возрастанию).
+
+Таблицы: Sales.Orders, Sales.OrderLines, Sales.Customers.
+*/
+
+select	o.OrderID, 
+		format(o.OrderDate, 'dd.MM.yyyy') OrderDate, 
+		datename(month, o.OrderDate) 'Month', 
+		datename(quarter, o.OrderDate) 'Quarter', 
+		round(datepart(month, o.OrderDate)/4, 0) + 1 'Season', 
+		c.CustomerName 
+from Sales.Orders o
+inner join Sales.OrderLines ol on o.OrderID = ol.OrderID
+inner join Sales.Customers c on o.CustomerID = c.CustomerID
+where (ol.UnitPrice > 100 or ol.Quantity > 20) and ol.PickingCompletedWhen is not null
+order by datename(quarter, o.OrderDate), round(datepart(month, o.OrderDate)/4, 0) + 1, format(o.OrderDate, 'dd.MM.yyyy')
+offset 1000 rows fetch first 100 rows only
+
+/*
+4. Заказы поставщикам (Purchasing.Suppliers),
+которые должны быть исполнены (ExpectedDeliveryDate) в январе 2013 года
+с доставкой "Air Freight" или "Refrigerated Air Freight" (DeliveryMethodName)
+и которые исполнены (IsOrderFinalized).
+Вывести:
+* способ доставки (DeliveryMethodName)
+* дата доставки (ExpectedDeliveryDate)
+* имя поставщика
+* имя контактного лица принимавшего заказ (ContactPerson)
+
+Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders, Application.DeliveryMethods, Application.People.
+*/
+
+select dm.DeliveryMethodName, po.ExpectedDeliveryDate, s.SupplierName, p.FullName 'ContactPerson' from Purchasing.Suppliers s
+inner join Purchasing.PurchaseOrders po on po.SupplierID = s.SupplierID
+inner join Application.DeliveryMethods dm on dm.DeliveryMethodID = po.DeliveryMethodID
+inner join Application.People p on p.PersonID = po.ContactPersonID
+where po.ExpectedDeliveryDate between '20130101' and '20130131' and dm.DeliveryMethodName in ('Air Freight', 'Refrigerated Air Freight') and po.IsOrderFinalized = 1
+order by po.ExpectedDeliveryDate
+
+/*
+5. Десять последних продаж (по дате продажи) с именем клиента и именем сотрудника,
+который оформил заказ (SalespersonPerson).
+Сделать без подзапросов.
+*/
+
+select top 10 p.FullName 'CustomerName', p2.FullName 'SalesPerson', o.* from Sales.Orders o
+inner join Application.People p on p.PersonID = o.CustomerID
+inner join Application.People p2 on p2.PersonID = o.SalespersonPersonID
+order by o.OrderDate desc
+
+/*
+6. Все ид и имена клиентов и их контактные телефоны,
+которые покупали товар "Chocolate frogs 250g".
+Имя товара смотреть в таблице Warehouse.StockItems.
+*/
+
+select p.PersonID, p.FullName, p.PhoneNumber from Sales.Orders o
+inner join Sales.OrderLines ol on o.OrderID = ol.OrderID
+inner join Application.People p on o.CustomerID = p.PersonID
+where ol.StockItemID = 224 
+
+
+select p.PersonID, p.FullName, p.PhoneNumber from Sales.Orders o
+inner join Sales.OrderLines ol on o.OrderID = ol.OrderID
+inner join Application.People p on o.CustomerID = p.PersonID
+inner join Warehouse.StockItems si on ol.StockItemID = si.StockItemID 
+									and si.StockItemName = 'Chocolate frogs 250g'
