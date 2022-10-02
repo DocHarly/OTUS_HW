5b7ee6c6d38faf9fd9da4e60f3ba69d844284694
Alexander Treboganov <docharly@yandex.ru>
2022-10-03 00:36:13 +0300

    Update HW-03-Union.sql

--- HW3-Union/HW-03-Union.sql
+++ HW3-Union/HW-03-Union.sql
@@ -80,7 +80,21 @@ order by datename(year, i.InvoiceDate)
 Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
 */
 
-напишите здесь свое решение
+select datename(year, i.InvoiceDate) 'Year'
+	,datename(month, i.InvoiceDate) 'Month'
+	,si.StockItemName
+	,sum(il.Quantity) 'Quantity'
+	,sum(il.ExtendedPrice) 'TotalPrice'
+	,min(i.InvoiceDate) 'FirstDate'
+from Sales.Invoices i
+inner join Sales.InvoiceLines il on i.InvoiceID = il.InvoiceID
+inner join Warehouse.StockItems si on il.StockItemID = si.StockItemID
+group by datename(year, i.InvoiceDate)
+	,datename(month, i.InvoiceDate)
+	,si.StockItemName
+having sum(il.Quantity) < 50
+order by datename(year, i.InvoiceDate)
+	,datename(month, i.InvoiceDate)
 
 -- ---------------------------------------------------------------------------
 -- Опционально
@@ -88,10 +102,83 @@ order by datename(year, i.InvoiceDate)
 /*
 Написать запросы 2-3 так, чтобы если в каком-то месяце не было продаж,
 то этот месяц также отображался бы в результатах, но там были нули.
+
+2. Отобразить все месяцы, где общая сумма продаж превысила 4 600 000
 */
 
+with all_dates
+as (
+	select min(i.InvoiceDate) f_d
+		,max(i.InvoiceDate) l_d
+	from Sales.Invoices i
+	
+	union all
+	
+	select dateadd(month, 1, f_d)
+		,l_d
+	from all_dates
+	where f_d < l_d
+	)
+select datename(year, ad.f_d) 'Year'
+	,datename(month, ad.f_d) 'Month'
+	,i.[Total]
+from all_dates ad
+left join (
+	select year(i.InvoiceDate) 'Year'
+		,month(i.InvoiceDate) 'Month'
+		,sum(il.ExtendedPrice) 'Total'
+	from Sales.Invoices i
+	inner join Sales.InvoiceLines il on i.InvoiceID = il.InvoiceID
+	group by year(i.InvoiceDate)
+		,month(i.InvoiceDate)
+	having sum(il.ExtendedPrice) > 4600000
+	) i on i.[Year] = year(ad.f_d)
+	and i.[Month] = month(ad.f_d)
+order by year(ad.f_d)
+	,month(ad.f_d)
 
-select *
-from Sales.Invoices i
-left join Sales.InvoiceLines il on i.InvoiceID = il.InvoiceID and i.InvoiceDate between '20130101' and '20160101'
-where il.InvoiceID is null
+/* 
+3. Вывести сумму продаж, дату первой продажи
+и количество проданного по месяцам, по товарам,
+продажи которых менее 50 ед в месяц.
+Группировка должна быть по году,  месяцу, товару.
+*/
+
+with all_dates
+as (
+	select min(i.InvoiceDate) f_d
+		,max(i.InvoiceDate) l_d
+	from Sales.Invoices i
+	
+	union all
+	
+	select dateadd(month, 1, f_d)
+		,l_d
+	from all_dates
+	where f_d < l_d
+	)
+select datename(year, ad.f_d) 'Year'
+	,datename(month, ad.f_d) 'Month'
+	,i.StockItemName
+	,i.Quantity
+	,i.TotalPrice
+	,i.FirstDate
+from all_dates ad
+left join (
+	select year(i.InvoiceDate) 'Year'
+		,month(i.InvoiceDate) 'Month'
+		,si.StockItemName
+		,sum(il.Quantity) 'Quantity'
+		,sum(il.ExtendedPrice) 'TotalPrice'
+		,min(i.InvoiceDate) 'FirstDate'
+	from Sales.Invoices i
+	inner join Sales.InvoiceLines il on i.InvoiceID = il.InvoiceID
+	inner join Warehouse.StockItems si on il.StockItemID = si.StockItemID
+	group by year(i.InvoiceDate)
+		,month(i.InvoiceDate)
+		,si.StockItemName
+	having sum(il.Quantity) < 50
+	) i on i.[Year] = year(ad.f_d)
+	and i.[Month] = month(ad.f_d)
+order by year(ad.f_d)
+	,month(ad.f_d)
